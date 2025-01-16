2881fd650029a1ddfb2fef390c489ab2262c14b3
Krlostks <carlospixel13@gmail.com>
2025-01-16 11:45:23 -0600

    01 Guitarra, Header, Footer Components

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+import  Header from './components/Header.vue'
+import  Footer from './components/Footer.vue'
+import { db } from './data/guitars'
+import Guitarra from './components/Guitarra.vue'
+
+const guitarras = ref(db)
+console.log(guitarras.value)
+
+</script>
+<template>
+  <Header/>
+
+  <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">        
+        <Guitarra v-for="guitarra in guitarras"  :guitarra="guitarra"/>
+        </div>
+  </main>
+
+  <Footer/>
+</template>
